a025254695bfb1c7d90acbd042459bc3f33405c5
Dag-c <diego.guillen.d.cruz@gmail.com>
2025-05-05 18:19:01 -0600

    change setup_ssl logic for deploy v:1.1

--- setup_ssl.sh
+++ setup_ssl.sh
@@ -4,33 +4,29 @@ set -a
 source .env
 set +a
 
-echo "🔁 Paso 1: Configuración HTTP temporal para obtener certificados..."
-export NGINX_CONF=nginx.http.conf
-sed -i 's/^NGINX_CONF=.*/NGINX_CONF=nginx.http.conf/' .env
-
-echo "🧹 Apagando servicios antiguos si existen..."
-docker-compose down
-
-echo "🐘 Levantando base de datos (postgres) y Redis..."
+echo "Stop old containers and their volumes..."
+docker-compose down -v
+sleep 5
+echo "Starting Postgres and Redis containers..."
 docker-compose up -d postgres redis
 
-echo "🚀 Levantando API..."
+echo "Starting API..."
 docker-compose up -d api
 
-echo "🌐 Levantando Nginx con configuración HTTP..."
+echo "Starting Nginx HTTP Config..."
+sed -i 's/^NGINX_CONF=.*/NGINX_CONF=nginx.http.conf/' .env
 docker-compose up -d nginx
 
-echo "⌛ Esperando 5 segundos para que Nginx esté listo..."
+echo "Waiting for Nginx..."
 sleep 5
 
-echo "🔐 Ejecutando Certbot (renovación no interactiva)..."
+echo "Run Certbot (renovación no interactiva)..."
 docker-compose run --rm certbot renew -v
 
 echo "🛑 Apagando Nginx (modo HTTP)..."
 docker-compose stop nginx
 
 echo "🔁 Paso 2: Cambiando a configuración HTTPS..."
-export NGINX_CONF=nginx.https.conf
 sed -i 's/^NGINX_CONF=.*/NGINX_CONF=nginx.https.conf/' .env
 
 echo "🚀 Levantando Nginx con configuración HTTPS..."
